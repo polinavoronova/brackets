66d6e1c23ef305774c23120a84e93c3b39e9757a
polina.voronova <polonium.pv@gmail.com>
2019-10-13 18:05:31 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let map = toMap(bracketsConfig);
+
+  let stack = [];
+
+  for (let char of str) {
+    if (map[char]) {
+
+      if (char == map[char]) {
+
+        if (stack[stack.length - 1] === char) {
+          stack.pop();
+        } 
+        else {
+          stack.push(char);
+        }
+
+      } 
+
+      else {
+        stack.push(char);
+      }
+
+    }
+    
+    else {
+      if (stack.length === 0) {
+        return false;
+      }
+      
+      let key = stack[stack.length - 1];
+      if (map[key] === char) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  if (stack.length !== 0) {
+    return false;
+  }
+  return true;
+}
+
+function toMap(config) {
+  let map = {};
+  
+  for (let rule of config) {
+    let key = rule[0];
+    let value = rule[1];
+    map[key] = value;
+  }
+  
+  return map;
 }
